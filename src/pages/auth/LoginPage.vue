44026db6668961610d4acbeb980d846f02d90e10
Edson Luiz <edsoncpimw@gmail.com>
2026-06-13 17:15:45 -0300

    Ativação do login com a tecla enter

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -5,54 +5,54 @@
         <div class="text-h5 text-weight-bold">Bem-vindo de volta</div>
         <div class="text-subtitle2 text-grey-6">Faça login para continuar</div>
       </q-card-section>
+      <q-form @keyup.enter="onSubmit">
+        <q-card-section class="q-gutter-md q-pt-none">
+          <q-input
+            v-model="form.email"
+            label="E-mail"
+            type="email"
+            outlined
+            :rules="rules.email"
+            :error="!!errorMessage"
+          >
+            <template #prepend>
+              <q-icon name="email" />
+            </template>
+          </q-input>
 
-      <q-card-section class="q-gutter-md q-pt-none">
-        <q-input
-          v-model="form.email"
-          label="E-mail"
-          type="email"
-          outlined
-          :rules="rules.email"
-          :error="!!errorMessage"
-        >
-          <template #prepend>
-            <q-icon name="email" />
-          </template>
-        </q-input>
+          <q-input
+            v-model="form.password"
+            label="Senha"
+            outlined
+            :type="showPassword ? 'text' : 'password'"
+            :rules="rules.password"
+            :error="!!errorMessage"
+            :error-message="errorMessage"
+          >
+            <template #prepend>
+              <q-icon name="lock" />
+            </template>
+            <template #append>
+              <q-icon
+                :name="showPassword ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="showPassword = !showPassword"
+              />
+            </template>
+          </q-input>
+        </q-card-section>
 
-        <q-input
-          v-model="form.password"
-          label="Senha"
-          outlined
-          :type="showPassword ? 'text' : 'password'"
-          :rules="rules.password"
-          :error="!!errorMessage"
-          :error-message="errorMessage"
-        >
-          <template #prepend>
-            <q-icon name="lock" />
-          </template>
-          <template #append>
-            <q-icon
-              :name="showPassword ? 'visibility_off' : 'visibility'"
-              class="cursor-pointer"
-              @click="showPassword = !showPassword"
-            />
-          </template>
-        </q-input>
-      </q-card-section>
-
-      <q-card-section class="q-pt-none">
-        <q-btn
-          label="Entrar"
-          color="primary"
-          class="full-width"
-          size="lg"
-          :loading="loading"
-          @click="onSubmit"
-        />
-      </q-card-section>
-
+        <q-card-section class="q-pt-none">
+          <q-btn
+            label="Entrar"
+            color="primary"
+            class="full-width"
+            size="lg"
+            :loading="loading"
+            @click="onSubmit"
+          />
+        </q-card-section>
+      </q-form>
       <div class="text-center q-pb-md">
         <span class="text-grey-6">Não tem conta? </span>
         <router-link to="/auth/register" class="text-primary">Criar conta</router-link>
